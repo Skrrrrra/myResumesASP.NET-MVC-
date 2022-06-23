332e53ea2f959deca17aef53add5d0eaf61c2903
Илья Чернышев <kseniaakvilonova@gmail.com>
2022-06-23 22:33:17 +0300

    Добавьте файлы проекта.

---
+++ myResumes/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myResumes.Models;
+using System.Diagnostics;
+
+namespace myResumes.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            ViewBag.Title = "My resumes";
+            return View();
+        }
+
+        public ViewResult aboutMePage()
+        {
+            return View();
+        }
+
+    }
+}
